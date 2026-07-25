195979b476bcfd3204b152f87b7b1c9306123530
Massena Beryl <massenajonas256@gmail.com>
2026-07-24 21:00:57 -0400

    Add sitemap.xml

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    Sitemap({
+      hostname: 'https://berylimages.com',
+      dynamicRoutes: [
+        '/',
+      ],
+    }),
   ],
+
   server: {
     allowedHosts: [
       'errand-upcountry-paver.ngrok-free.dev'
     ]
   },
+
   preview: {
     host: true,
     allowedHosts: [
